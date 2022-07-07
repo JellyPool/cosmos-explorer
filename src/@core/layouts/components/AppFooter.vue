372db3036fd7a5396fb056fe93e69bf5526ca432
Mattia Failla <mattiafailla@hotmail.it>
2022-07-07 18:44:10 +0200

    Switch to dark theme

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,30 +1,17 @@
 <template>
   <p class="clearfix mb-0">
     <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">
-      Powered By
+      Based on the work of
       <b-link
         class="ml-25 font-weight-bolder"
         href="#"
         target="_blank"
-      >Ping.pub</b-link>
+      >Ping.pub</b-link> | Brought to you by <b-link
+        class="ml-25 font-weight-bolder"
+        href="https://www.jellypool.xyz/"
+        target="_blank"
+      >Jelly</b-link>
     </span>
-
-    <router-link
-      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-      class="float-md-right"
-      to="/coffee"
-    >Buy me a cup of coffee.
-      <span
-        v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-        variant="outline-primary"
-      >
-        <feather-icon
-          icon="HeartIcon"
-          size="21"
-          class="text-danger stroke-current"
-        />
-      </span>
-    </router-link>
   </p>
 </template>
 
